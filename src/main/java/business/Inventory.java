60e7c755759348b7c8ee845a0da61de6ad9a1ad3
alexanderjaramillo <alexunjm@gmail.com>
2020-04-14 02:28:52 -0500

    agregar productos al inventario

--- src/main/java/business/Inventory.java
+++ src/main/java/business/Inventory.java
@@ -1,14 +1,11 @@
 package business;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Inventory {
 	
 	private static Inventory instance;
-	
-	private int size;
-	
-	public Inventory() {
-		this.size = 0;
-	}
 
 	static Inventory getInstance() {
 		if (instance == null) {
@@ -16,17 +13,49 @@ public class Inventory {
 		}
 		return instance;
 	}
+	
+	private Map<String, Integer> items;
+	
+	public Inventory() {
+		this.items = new HashMap<String, Integer>();
+		this.clearStock();
+	}
+	
+	private void add(String product, int newQuantity) {
+		if (this.items.containsKey(product)) {
+			Integer quantity = this.items.get(product);
+			this.items.put(product, quantity + newQuantity);
+		} else {
+			this.items.put(product, newQuantity);
+		}
+	}
 
-	public Object productsInStock() {
-		return size;
+	public void clearStock() {
+		this.items.clear();
+	}
+
+	public int productsInStock() {
+		return this.items.size();
 	}
 
 	public boolean addStock(String name) {
+		return this.addStock(name, 1);
+	}
+
+	public boolean addStock(String name, int quantity) {
 		if (name == null || name.trim().equals("")) {
 			return false;
 		}
-		this.size++;
+		add(name, quantity);
 		return true;
+	}
+
+	public int productsInStockOf(String productName) {
+		return this.items.get(productName);
+	}
+
+	public Object[] arrayOfProducts() {
+		return this.items.keySet().toArray();
 	}
 
 }
--- src/test/java/business/InventoryTest.java
+++ src/test/java/business/InventoryTest.java
@@ -1,11 +1,18 @@
 package business;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -17,32 +24,58 @@ public class InventoryTest {
 	public void setup() {
 		this.inventory = Inventory.getInstance();
 	}
-
-	@Test
-	public void testSingleton() {
-		assertNotNull(this.inventory);
-		assertSame(this.inventory, Inventory.getInstance());
+	
+	@After
+	public void clearData() {
+		this.inventory.clearStock();
 	}
 
 	@Test
-	public void testProductsInStockIsZero() {
+	public void testUniqueInstanceOfInventory() {
+		assertNotNull(this.inventory);
+		assertSame(this.inventory, Inventory.getInstance());
+		assertEquals(this.inventory, Inventory.getInstance());
+	}
+
+	@Test
+	public void testCreateEmptyProductReturnFalseAndSizeNoChange() {
+		assertEquals(0, this.inventory.productsInStock());
+		assertFalse(this.inventory.addStock(null));
+		assertFalse(this.inventory.addStock(""));
+		assertFalse(this.inventory.addStock(null, 2));
+		assertFalse(this.inventory.addStock("", 3));
 		assertEquals(0, this.inventory.productsInStock());
 	}
 
 	@Test
-	public void testCreateEmptyProductReturnFalse() {
-		assertFalse(this.inventory.addStock(null));
-		assertFalse(this.inventory.addStock(""));
-	}
-
-	@Test
-	public void testCreateProductReturnTrue() {
+	public void testCreateProductAndCheckStockSize() {
+		assertEquals(0, this.inventory.productsInStock());
 		assertTrue(this.inventory.addStock("Vaso Hulk"));
-	}
-
-	@Test
-	public void testProductsInStockIsOne() {
 		assertEquals(1, this.inventory.productsInStock());
 	}
 
+	@Test
+	public void testNewSingleItemArrayStock() {
+		assertEquals(0, this.inventory.productsInStock());
+		List<Object> products = Arrays.asList(new Object[] {"Vaso", "Camiseta", "Vaso", "Comic", "Vaso", "Vaso", "Juguete", "Juguete"});
+		for (Object product : products) {
+			assertTrue(this.inventory.addStock((String) product));
+		}
+		Set<Object> setOfProducts = new HashSet<Object>(products);
+		assertEquals(setOfProducts.size(), this.inventory.productsInStock());
+		assertArrayEquals(setOfProducts.toArray(), this.inventory.arrayOfProducts());
+		assertEquals(4, this.inventory.productsInStockOf("Vaso"));
+	}
+
+	@Test
+	public void testSupplyInventory() {
+		assertEquals(0, this.inventory.productsInStock());
+		List<Object[]> products = Arrays.asList(new Object[][] {
+			{"Vaso", 40}, {"Camiseta", 2}, {"Comic", 1}, {"Juguete", 2}
+			});
+		for (Object[] product : products) {
+			assertTrue(this.inventory.addStock((String) product[0], (int) product[1]));
+		}
+		assertEquals(2, this.inventory.productsInStockOf("Juguete"));
+	}
 }
